c20405f441655715a0fdc6a8e53be2c970ab3516
Reid Campolong <reidcampolong@gmail.com>
2018-09-28 14:11:51 -0400

    Indexes Menus, Searches for Chinese

--- chinese.py
+++ chinese.py
@@ -4,46 +4,96 @@ Running this program will search all the RIT dining locations and try
 to find me some delicious chinese food.
 
 @author - Reid
+9/28/18
 """
 
 from bs4 import BeautifulSoup
 import requests
 
 def findChineseFood() :
-    base_url = "https://www.rit.edu/fa/diningservices/"
+    print("Let's find chinese food!")
+    #print(analyzePages())
+    placesWithChineseFood = []
+    analyzedPages = analyzePages()
+    for store in analyzedPages:
+        print("================")
+        print("LOCATION: " + store[0])
+        print("")
+        for menuItem in store[1:]:
+            if(_analyzeForChineseFood(menuItem)):
+                placesWithChineseFood.append(store[0])
+            print(menuItem)
+    print("HAS CHINESE FOOD: " + str(placesWithChineseFood))
 
-    #page_response = requests.get(base_url+"commons", timeout=5)
-    #page_content = BeautifulSoup(page_response.content, "html.parser")
 
-    #menu_items = page_content.find_all(class_="menu-items")
+def analyzePages() -> list:
+    """
+    Scrape the RIT dining page and get a list of all dining locations
+    :return:
+    """
 
-    #print(menu_items)
-    print(analyzePages())
-
-def analyzePages() -> str:
     base_url = "https://www.rit.edu/fa/diningservices/places-to-eat/hours"
-    page_response = requests.get(base_url, timeout=5)
+    print("Retrieving dining locations from " + base_url)
+
+    page_response = requests.get(base_url, timeout=2)
     page_content = BeautifulSoup(page_response.content, "html.parser")
 
     list_of_places = str(page_content.find_all(class_="hours-title"))
-    #print(list_of_places)
     list_of_urls = list_of_places.split("<a")
-    for link in list_of_urls[1:]:
-        #print(link)
-        analyzePage(_extractLinkFromText(link))
-    #print(list_of_places)
 
-def analyzePage(url) -> str:
-    page_response = requests.get(url, timeout=5)
+    print("Successful! Analyzing the locations now.")
+
+    menuItems = []
+    for page in list_of_urls[1:]:
+        diningPage = _extractLinkFromText(page)
+        print("Checking " + diningPage)
+        menuItems.append(analyzePage(diningPage))
+    return menuItems
+
+def analyzePage(url) -> list:
+    """
+    Scrape a SINGLE RIT dining page and get a list of food
+    :param url:
+    """
+    page_response = requests.get(url, timeout=2)
     page_content = BeautifulSoup(page_response.content, "html.parser")
 
-    menu_items = page_content.find_all(class_="menu-items")
+    locationName = str(page_content.find("title"))
+    locationName = _modifyForeignChars(locationName.replace("<title>", "")\
+        .replace("</title>","").replace(" | Dining Services", ""))
 
-    print(menu_items)
+    print("Menu items have been retrieved.")
+    menu_item_string = str(page_content.find_all(class_="menu-items"))
+    raw_menu_items = menu_item_string.split('<div')
+
+    menuItems = [locationName]
+    print(menuItems)
+    for itemString in raw_menu_items[1:]:
+        menuItems.append(_analyzeItemFromMenuString(itemString))
+    return menuItems
+
+def _analyzeItemFromMenuString(menu_item_string) -> str:
+    startIndex = menu_item_string.index('-items">') + 7
+    endIndex = menu_item_string.rfind("<br",)
+    menuItem = menu_item_string[startIndex+1:endIndex]
+    return _modifyForeignChars(menuItem)
+
+def _modifyForeignChars(text) -> str:
+    text = text.replace("<br/>", ", ")
+    text = text.replace("&amp;", "&")
+    return text
 
 def _extractLinkFromText(text) -> str:
+    """
+    Extract a dining link from a long string list
+    :param text:
+    :return:
+    """
     startIndex = text.index('href="') + 5
     endIndex = text.index('\"', startIndex + 1)
     return text[startIndex + 1:endIndex]
+
+def _analyzeForChineseFood(menuItem) -> bool:
+    return 'Chinese' in menuItem
 
 findChineseFood()
